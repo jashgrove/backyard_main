eb6393f4c590d2c7d45912c1aaa0a51f73491710
PierrePaul Guertin <pierrepaul.guertin@gmail.com>
2025-02-24 16:51:21 -0500

    Added methods to the listings

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -3,4 +3,27 @@ class ListingController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
 end
