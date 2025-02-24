241d8a8dc833ed582f6324f4950ae6a86ead4101
Lorena Davila <lorena.davila.ermus@gmail.com>
2025-02-24 16:25:00 -0500

    added the initial show listings view

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,2 +1,6 @@
 class ListingController < ApplicationController
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,8 @@
 class Listing < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true, uniqueness: true
+  validates :price_per_hour, presence: true, numericality: { greater_than: 0 }
+  validates :user, presence: true
 end
