a34f8ea17badf882433ce2b2890f53ad82a9fec3
Dan Vy Ho <ho.danvy@gmail.com>
2025-02-24 16:57:53 -0500

    creating new listings and views

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,3 +1,18 @@
 class ListingController < ApplicationController
 
+def new
+  @listing = Listing.find(params[:id])
+end
+
+def create
+  @listing = Listing.create(listing_params)
+  @listing.save
+  redirect_to  listing_path
+end
+
+private
+
+def listing_params
+  params.require(:listing).permit(:name :description :address :price_per_hour)
+end
 end
