a013d113aea1fe23b67a3db7165b33d52c2ecc03
Lorena Davila <lorena.davila.ermus@gmail.com>
2025-02-24 18:17:50 -0500

    make some fixes for the show view to render

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
 
-  has_many :bookings, :listing_reviews
-
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true
   validates :price_per_hour, presence: true, numericality: { greater_than: 0 }
