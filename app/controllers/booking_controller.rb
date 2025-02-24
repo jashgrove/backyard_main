a4c8de273b52ce69a2f7b7b691fbd758d7442f34
Jasper Ashgrove <jasperashgrove@gmail.com>
2025-02-24 16:54:30 -0500

    fixed booking controllers, accidentally put them in the listings controller.

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,2 +1,15 @@
 class BookingController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @user = Current.user
+    @listing = Listing.find(params[:id])
+    @booking = Booking.create(listing_params)
+    @booking.user = @user
+    @booking.listing = @listing
+    @booking.save
+  end
 end
--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,13 +1,3 @@
 class ListingController < ApplicationController
 
-  def new
-    @listing = Listing.new
-  end
-
-  def create
-    @user = User.find(params[:id])
-    @listing = Listing.create(listing_params)
-    @listing.user = @user
-    @listing.save
-  end
 end
