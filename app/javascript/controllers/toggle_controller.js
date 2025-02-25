716b528eb25cc5c5e8768950e6f9c18bfd6042b6
Dan Vy Ho <ho.danvy@gmail.com>
2025-02-25 17:01:30 -0500

    you can create a new listing and it is inserted into the current list. You are also able to toggle the create listing button

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,6 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["togglableElement"]
+
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
   }
