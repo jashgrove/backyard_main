e3b64a56b549a027f587943c41d3466ce8945e7c
Jasper <jasperashgrove@gmail.com>
2025-02-25 11:44:45 -0500

    Update and rename listing_controller.rb to listings_controller.rb
    Remove redundancies

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,24 @@
+class ListingsController < ApplicationController
+
+  def new
+    @listing = Listing.new
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @listing = Listing.create(listing_params)
+    @listing.user = @user
+    @listing.save
+    redirect_to  listing_path
+  end
+
+private
+
+  def listing_params
+    params.require(:listing).permit(:name :description :address :price_per_hour)
+  end
+end
