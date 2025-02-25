5b0f21e1d9ddbc18b51b3646f9a2fa7584a5b12d
Dan Vy Ho <ho.danvy@gmail.com>
2025-02-25 12:03:22 -0500

    listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,23 +1,4 @@
-<<<<<<< HEAD:app/controllers/listing_controller.rb
-class ListingController < ApplicationController
-  def index
-    @listings = Listing.all
-    @listing = Listing.new
-  end
-
-  def new
-    @listing = Listing.new
-  end
-
-  def create
-    @user = User.find(params[:id])
-    @listing = Listing.create(listing_params)
-    @listing.user = @user
-    @listing.save
-=======
 class ListingsController < ApplicationController
->>>>>>> 3ceebe987836f9df1991ae23db2c02642c7bd689:app/controllers/listings_controller.rb
-
   def show
     @listing = Listing.find(params[:id])
   end
@@ -36,6 +17,6 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-  params.require(:listing).permit(:name :description :address :price_per_hour)
+    params.require(:listing).permit(:name, :description, :address, :price_per_hour)
   end
 end
