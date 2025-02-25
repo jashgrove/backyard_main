4952d4aecc51817aec0cc4c6e29c9c256b09654a
Dan Vy Ho <ho.danvy@gmail.com>
2025-02-25 16:27:45 -0500

    pulled

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,12 +1,6 @@
 
 export default class extends Controller {
   static targets = ["togglableElement"]
-
-  static targets = ["togglableElement"]
-  fire() {
-    this.togglableElementTarget.classList.toggle("d-none");
-  }
-
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
   }
