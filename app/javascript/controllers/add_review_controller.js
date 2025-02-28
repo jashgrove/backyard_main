71b8e1d7b433999aeea3ea10732822e0134fda2f
Lorena Davila <lorena.davila.ermus@gmail.com>
2025-02-28 10:51:10 -0500

    added reviews validation and prevent booking if no dates entered

--- app/javascript/controllers/add_review_controller.js
+++ app/javascript/controllers/add_review_controller.js
@@ -3,13 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="add-review.js"
 export default class extends Controller {
   connect() {
+    // this.element.addEventListener("submit", this.validate.bind(this))
   }
 
-  static targets = ["addReviewForm"]
+  static targets = ["addReviewForm", "rating"]
 
   showForm() {
     this.addReviewFormTarget.classList.remove("d-none");
   }
+
+  // validate(event) {
+  //   const rating = this.ratingTarget.value
+  //   if (rating.trim() === "" || ![1,2,3,4,5].includes(parseInt(rating))) {
+  //     event.preventDefault()
+  //   }
+  // }
 
   // addReview(event) {
   //   event.preventDefault()
